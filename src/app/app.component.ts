aefcc4158c06eb5b7ce57fdc66a0e7548c4c3d2a
Daniel <daniel.o.jones@gmail.com>
2020-10-29 19:52:41 -0400

    Issue 35: Navigate with sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,63 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  // This is a comment for Austin's milestone.
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
+export class AppComponent {currentPageTitle = 'Dashboard';
+
+appPages = [
+  {
+    title: 'Home',
+    url: '/pages/home',
+    icon: 'home'
+  },
+  {
+    title: 'Avaliable Lots',
+    url: '/pages/avaliable-lots',
+    icon: 'film'
+  },
+  {
+    title: 'All lots',
+    url: '/pages/all-lots',
+    icon: 'settings'
+  },
+  {
+    title: 'Manage Passes',
+    url: '/pages/manage-passes',
+    icon: 'clipboard'
+  },
+  {
+    title: 'Purchase Pass',
+    url: '/pages/purchase-a-pass',
+    icon: 'card'
+  },
+  {
+    title: 'Settings',
+    url: '/pages/settings',
+    icon: 'settings'
+  },
+  {
+    title: 'Map',
+    url: '/pages/map',
+    icon: 'map'
+  },
+  {
+    title: 'Analytics',
+    url: '/pages/analytics',
+    icon: 'bar-chart'
+  },
+  {
+    title: 'Favorites',
+    url: '/pages/favorites',
+    icon: 'star'
   }
+];
+
+constructor(
+  private platform: Platform,
+  private splashScreen: SplashScreen,
+  private statusBar: StatusBar,
+) {
+  this.initializeApp();
+}
 
   initializeApp() {
     this.platform.ready().then(() => {
