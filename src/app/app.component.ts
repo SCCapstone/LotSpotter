473f92f5f3f3c9ceb7c20cbed58fcca6f9f0d8a3
aj-staton <ajstaton0@gmail.com>
2020-11-01 12:30:52 -0500

    issue #14, I've added a map with various parking locations. Will need to expand to more lots on campus, potentially reading from a backend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Environment } from '@ionic-native/google-maps';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  // This is a comment for Austin's milestone.
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -21,6 +22,13 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      Environment.setEnv({
+        // api key for server
+        'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyA4gpqIrlhwjFpfkqm2e2lnnm-xxbJZXMQ',
+
+        // api key for local development
+        'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyA4gpqIrlhwjFpfkqm2e2lnnm-xxbJZXMQ'
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
