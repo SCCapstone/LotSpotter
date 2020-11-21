9294924733873dff7381cd790bf4756ee3200ab7
adelnoce <abigaildelnoce@gmail.com>
2020-11-21 14:15:03 -0500

    working on pulling from firebase

--- src/app/pages/all-lots/all-lots.page.ts
+++ src/app/pages/all-lots/all-lots.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-all-lots',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllLotsPage implements OnInit {
 
-  constructor() { }
+  tasks:any = [];
+
+  constructor(
+    private afFirestore: AngularFirestore
+  ) { }
+
+  fetch() {
+    this.afFirestore.collection('lots').snapshotChanges().subscribe(res => {
+      console.log(res);
+      let tmp = [];
+      res.forEach(task => {
+        tmp.push({ key: task.payload.doc.id, ...task.payload.doc.data });
+      })
+      console.log(tmp);
+      this.tasks = tmp;
+    })
+  }
 
   ngOnInit() {
   }
