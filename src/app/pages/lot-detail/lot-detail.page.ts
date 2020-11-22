525538b6ae409dfe674114a09edbbe5f93f0c463
aj-staton <ajstaton0@gmail.com>
2020-11-21 22:59:15 -0500

    working on html

--- src/app/pages/lot-detail/lot-detail.page.ts
+++ src/app/pages/lot-detail/lot-detail.page.ts
@@ -13,6 +13,11 @@ import { Lot } from '../../interfaces';
 })
 export class LotDetailPage implements OnInit {
  
+  private map = "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap \
+  &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318 \
+  &markers=color:red%7Clabel:C%7C40.718217,-73.998284 \
+  &key=AIzaSyA4gpqIrlhwjFpfkqm2e2lnnm-xxbJZXMQ"
+
   private currentLot:Lot = {
     name: "...",
     addr: "...",
@@ -31,11 +36,15 @@ export class LotDetailPage implements OnInit {
     let param:string = this.route.snapshot.paramMap.get("name"); // src: https://www.youtube.com/watch?v=DZrWzoW4_4M
     this.backend.getLotData(param).then((res) => {
       this.currentLot = res;
+      // Call the image API request.
     }).catch((message) => {
       console.log("Could not get lot data.")
     });
     console.log("Viewing Lot: " + param);
   }
 
+  getImage() {
+
+  }
 
 }
