2043708a6ae21c3e100c02a2f90c6093b1356af4
Cassidy <cmb11@email.sc.edu>
2020-11-22 22:48:30 -0500

    Form created for Testing UI

--- src/app/pages/testing-ui/testing-ui.page.ts
+++ src/app/pages/testing-ui/testing-ui.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import firebase from 'firebase';
+import { Lot } from '../../interfaces';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-testing-ui',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestingUiPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  private database = firebase.firestore();
+  private lots:Lot[] = [];
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { 
+    this.form = formBuilder.group({
+      lot:['', Validators.required],
+      operation:['',Validators.required],
+      number_of_vehicles:['', Validators.required],
+    })
   }
 
+  ngOnInit() {
+    this.fetch();
+  }
+
+  fetch() {
+    var self = this;
+    this.database.collection('lots').onSnapshot(function(querySnapshot) {
+      self.lots = [];
+      querySnapshot.forEach(function(doc) {
+        let a = doc.data();
+        let lot:Lot = { name: a.name,
+                addr: a.addr,
+                currCap: a.currCap,
+                maxCap: a.maxCap,
+                desc: a.desc,
+                loc: a.loc,
+                lotType: a.lotType
+        }
+        self.lots.push(lot);
+      });
+    });
+  }
+
+  // Plans for later on:
+  //   Add in error checking so that user can't assign more open spots than the lot/garage max occupancy
+  //   Add in error checking to make sure user enters in numbers
+  //   Display the current number of open spots and max occupancy, maybe make it reactive to reflect what
+  //     user is inputing
+  //   Make it change the firebase currCap
 }
