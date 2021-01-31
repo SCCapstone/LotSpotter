a8a79bbffb7cb76c9647b767ba7822be291b161b
Daniel <daniel.o.jones@gmail.com>
2021-01-31 14:27:53 -0500

    More progress on login e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,30 @@ describe('new App', () => {
     browser.get("/")
   });
 
-  it('should have a login screen', () => {
+  it('should launch to a login screen and navigate to the homepage upon successful login', () => {
+
+    var emailEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input'));
+    var passwordEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[2]/ion-input/input'));
+    var loginEl = element(by.css('ion-row.md:nth-child(3) > ion-col:nth-child(1) > ion-button:nth-child(1)'));
+    //var homeTitleEl = element(by.css('.toolbar-title')).getAttribute('value');
     
-   element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input')).clear().then(() => {element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input')).sendKeys('test@test.com')})
-   //element(by.id("ion-input-1-lbl")).clear().then(() => {element(by.id("ion-input-1-lbl")).sendKeys('testtest')})
+   emailEl.clear().then(() => {
+     emailEl.sendKeys('test@test.com')
+        })
+
+        browser.sleep(5000);
+   passwordEl.clear().then(() => {
+    passwordEl.sendKeys('testtest')
+   })     
+
+        browser.sleep(5000)
+   loginEl.click()
+        browser.sleep(5000)
+
+   //expect(element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-home/ion-header/ion-toolbar/ion-title')).isPresent()).toBe(true);
+
+
+  
    
   });
 });
