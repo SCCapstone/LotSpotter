3a10aa861f422410de4a5e44cb1e8d0b0690b948
aj-staton <ajstaton0@gmail.com>
2021-01-30 23:38:38 -0500

    added test framework

---
+++ src/app/tests/unit-testing.service.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+
+describe('UnitTests', () => {
+
+  /* beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UnitTestingService);
+  }); */
+
+  it('lot data should be valid', () => {
+    expect(true).toBeTruthy();
+  });
+});
