094cc24c13ebe86d08061b18218a158c48dabfc4
Daniel <daniel.o.jones@gmail.com>
2021-01-31 06:14:19 -0500

    Began E2E testing with protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,17 @@
+import { browser, element, by } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    browser.get("/")
   });
 
-  it('should be blank', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
+  it('should have a login screen', () => {
+    
+   element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input')).clear().then(() => {element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input')).sendKeys('test@test.com')})
+   //element(by.id("ion-input-1-lbl")).clear().then(() => {element(by.id("ion-input-1-lbl")).sendKeys('testtest')})
+   
   });
 });
