7216b89d1c2240a705afd851b8ecdacb402f6500
AnadamaBread <lbaez133@gmail.com>
2021-01-31 15:16:52 -0500

    Consistancy check, prepare to merge BDD automation

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,43 +1,42 @@
 import { browser, element, by } from 'protractor';
 import { AppPage } from './app.po';
 
-describe('login', () => {
+describe('Login', () => {
   let page: AppPage;
 
   beforeEach(() => {
     browser.get("/")
   });
 
-  it('should launch to a login screen ', () => {
+  it('should launch to a login screen and navigate to the homepage upon successful login, allowing the user to logout', () => {
 
     var emailEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input'));
     var passwordEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[2]/ion-input/input'));
     var loginEl = element(by.css('ion-row.md:nth-child(3) > ion-col:nth-child(1) > ion-button:nth-child(1)'));
-    //var homeTitleEl = element(by.css('.toolbar-title')).getAttribute('value');
-    
+    var logOutEl = element(by.className('md button button-block button-solid ion-activatable ion-focusable hydrated'));
    emailEl.clear().then(() => {
      emailEl.sendKeys('test@test.com')
         })
 
-        browser.sleep(5000);
    passwordEl.clear().then(() => {
     passwordEl.sendKeys('testtest')
    })     
-
-        browser.sleep(5000)
    loginEl.click()
-        browser.sleep(5000)
+   
+   browser.get('/home');
+   logOutEl.click()
+   
 
-   //expect(element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-home/ion-header/ion-toolbar/ion-title')).isPresent()).toBe(true);
+
+
+
+
+   
+        
+   
+
+
   
    
-  });
-  it('navigates to the homepage upon successful login and logs out', () => {
-    // browser.refresh();
-    browser.get("/home")
-    var logOutEl = element(by.className('md button button-block button-solid ion-activatable ion-focusable hydrated'));
-    logOutEl.click();
-
-
   });
 });
