4fe7a52478e879a32aaeb42fba16657c3c07e40c
AnadamaBread <lbaez133@gmail.com>
2021-02-07 12:52:51 -0500

    Behavior Testing updates

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -51,5 +51,22 @@ describe('SignUp', () =>{
     createaccountEL.click();
     backbuttonEL.clear();
 
+    browser.get('/login');
+
+    var emailEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[1]/ion-input/input'));
+    var passwordEl = element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-login/ion-content/form/ion-item[2]/ion-input/input'));
+    var loginEl = element(by.css('ion-row.md:nth-child(3) > ion-col:nth-child(1) > ion-button:nth-child(1)'));
+    
+    emailEl.clear().then(() => {
+      emailEl.sendKeys('testdel@del.com')
+    })
+
+    passwordEl.clear().then(() => {
+      passwordEl.sendKeys('testtest')
+    })     
+    
+    loginEl.click()   
+    browser.get('/home');
+
   });
 });
