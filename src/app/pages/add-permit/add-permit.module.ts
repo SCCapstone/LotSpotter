cd0da09a1af0911fe1a36414f8178c46df485a28
aj-staton <ajstaton0@gmail.com>
2021-03-29 00:58:31 -0400

    user can now add thier own permits. works on #75.

--- src/app/pages/add-permit/add-permit.module.ts
+++ src/app/pages/add-permit/add-permit.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AddPermitPageRoutingModule } from './add-permit-routing.module';
 
 import { AddPermitPage } from './add-permit.page';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule,
+    IonicModule,
     FormsModule,
+    CommonModule,
     IonicModule,
     AddPermitPageRoutingModule
   ],
